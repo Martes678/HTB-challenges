a11813e8f48539416ccc423d5e6462b576d05724
Martes678 <manesortega@gmail.com>
2026-03-10 18:18:28 +0100

    Add files via upload

---
+++ xorxorxor/solve.py
@@ -0,0 +1,24 @@
+from binascii import unhexlify
+
+def solve():
+    hexflag = "134af6e1297bc4a96f6a87fe046684e8047084ee046d84c5282dd7ef292dc9"
+    
+    flag = unhexlify(hexflag)
+    
+    conocido = "HTB{"
+    clave = []
+    for i in range(4):
+        clave.append(flag[i] ^ ord(conocido[i]))
+    
+    print(f"Clave encontrada: {clave}")
+
+    descifrado = []
+    for i in range(len(flag)):
+        decoded_byte = flag[i] ^ clave[i % 4]
+        descifrado.append(chr(decoded_byte))
+    
+    texto = "".join(descifrado)
+    print(f"Flag: {texto}")
+
+if __name__ == "__main__":
+    solve()
